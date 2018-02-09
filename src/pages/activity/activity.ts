66949e40565903f698d8b65c810e9a5c5f600e94
robsonklima <robsonklima@gmail.com>
2018-02-09 11:20:30 -0200

    REF

--- src/pages/activity/activity.ts
+++ src/pages/activity/activity.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { RecomendationsPage } from '../recomendations/recomendations';
+import { RisksPage } from '../risks/risks';
 
 @Component({
   selector: 'activity-page',
@@ -11,6 +12,10 @@ export class ActivityPage {
   constructor(
     public navCtrl: NavController
   ) {}
+
+  public onOpenRisks() {
+    this.navCtrl.push(RisksPage);
+  }
 
   public onOpenRecomendations() {
     this.navCtrl.push(RecomendationsPage);
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ProjectsPage } from '../projects/projects';
+import { ActivitiesPage } from '../activities/activities';
+import { RisksPage } from '../risks/risks';
 
 @Component({
   selector: 'home-page',
@@ -13,5 +15,13 @@ export class HomePage {
 
   public onOpenProjects() {
     this.navCtrl.push(ProjectsPage);
+  }
+
+  public onOpenActivities() {
+    this.navCtrl.push(ActivitiesPage);
+  }
+
+  public onOpenRisks() {
+    this.navCtrl.push(RisksPage);
   }
 }
